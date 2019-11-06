5c00693e473ab9a7d6d406c9ff8531c2341a91e9
payalahuja98 <payalahuja98@gmail.com>
2019-11-06 13:37:11 -0600

    created template for reader and writer classes

---
+++ MemberReader.java
@@ -0,0 +1,12 @@
+package insurance_risk;
+/**
+ * 
+ * @author Payal Ahuja
+ * This class reads member data from a tab-delimited file.
+ *
+ */
+public class MemberReader {
+	public static void readData() {
+		
+	}
+}
---
+++ MemberWriter.java
@@ -0,0 +1,21 @@
+package insurance_risk;
+/**
+ * 
+ * @author Payal Ahuja
+ * This class is responsible for saving the members to
+ * a text file, a binary file, and an XML file.
+ *
+ */
+public class MemberWriter {
+	public static boolean writeToText() {
+		return false;
+	}
+	
+	public static boolean writeToBinary() {
+		return false;
+	}
+	
+	public static boolean writeToXML() {
+		return false;
+	}
+}

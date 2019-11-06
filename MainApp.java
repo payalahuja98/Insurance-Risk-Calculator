df994bff00e3c0f7d90d5a7f94cd38b82f6c190b
marilizh <aliceandmaryann@att.net>
2019-11-06 13:37:44 -0600

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainApp {
+	private static ArrayList<Member> members = new ArrayList<Member>();
+	private static void printStars(int starCount) {	
+		String starField ="";
+		for (int i = 0; i < starCount; i++) {
+			starField = starField + "*";
+		}
+		System.out.println(starField);
+	}
+	private static void printWelcome() {
+		printStars(53);
+		System.out.println("\tINSURANCE SCORE CARD");
+		System.out.println("  This app scores a potential customer");
+		System.out.println("  on various health attributes: blood");
+		System.out.println("  pressure, age, height, weight, and");
+		System.out.println("  family history of disease. It writes");
+		System.out.println("  each member's insurance grade to a");
+		System.out.println("  JSON file so that they can be easily");
+		System.out.println("  shared on a web-based data exchange.");
+		printStars(53);
+	}
+	//Menu 
+		public static void showMenu() {
+			System.out.println("\nHere are your choices:");
+			System.out.println("\t1. List members");
+			System.out.println("\t2. Add a new member");
+			System.out.println("\t3. Save members");
+			System.out.println("\t4. Load members");
+			System.out.println("\t5. Assess members");
+			System.out.println("\t6. Save assessments as JSON");
+			System.out.println("\t7. Exit");
+			System.out.print("Please enter your choice: ");
+		}
+		public static void addNewMember() {
+			Member newMember = new Member();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter first and last name: ");
+			String fullName = sc.nextLine();
+			String[] fullNameArr  = fullName.split(" ");
+			newMember.setFirstName(fullNameArr[0]);
+			newMember.setLastName(fullNameArr[1]);
+ 			System.out.println("Enter age");
+ 			newMember.setAge(sc.nextInt());
+			System.out.println("Enter height in inches: ");
+			newMember.setHeight(sc.nextInt());
+			System.out.println("Enter weight in pounds: ");
+			newMember.setWeight(sc.nextInt());
+			System.out.println("Enter blood pressure (sys and dia): ");
+			int bloodPressure = sc.nextInt();
+			//int[] bloodPressureArr = bloodPressure.split(" ");
+			System.out.println("Has a family member had ...");
+			System.out.println("Cancer?");
+			newMember.setHasCancer(sc.nextLine());
+			System.out.println("Diabetes?");
+			newMember.setHasDiabetes(sc.nextLine());
+			System.out.println("Alzheimers?");
+			newMember.setHasAlzheimers(sc.nextLine());
+			members.add(newMember);
+			System.out.println("The new member has been added.");
+			
+		}
+		public static void main(String[] args) {
+			printWelcome();
+			Scanner sc = new Scanner(System.in);
+			int choice;
+			System.out.print("Enter name of member file: ");
+			String fileName = sc.nextLine();
+			do {
+				showMenu();
+				choice = sc.nextInt();
+				if (choice == 1) {
+					//List members from the file
+					
+				}
+				if (choice == 2) {
+					//Add a new member
+					addNewMember();
+				}
+				if (choice == 3) {
+					//Save members
+				}
+				if (choice == 4) {
+					//Load members
+				}
+				if (choice == 5) {
+					//Assess members
+				}
+				if (choice == 6) {
+					//Save assessments as JSON
+				}
+				
+			} while (choice != 7);
+		}
+}
---
+++ Member.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+public class Member {
+	private String firstName;
+	private String lastName;
+    private int age;
+    private int height;
+    private int weight;
+    private int bPSyst;
+    private int bPDias;
+    private String hasCancer;
+    private String hasDiabetes;
+    private String hasAlzheimers;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String author) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String author) {
+        this.lastName = lastName;
+    }
+    public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getbPSyst() {
+		return bPSyst;
+	}
+	public void setbPSyst(int bPSyst) {
+		this.bPSyst = bPSyst;
+	}
+	public int getbPDias() {
+		return bPDias;
+	}
+	public void setbPDias(int bPDias) {
+		this.bPDias = bPDias;
+	}
+	public String getHasCancer() {
+		return hasCancer;
+	}
+	public void setHasCancer(String hasCancer) {
+		this.hasCancer = hasCancer;
+	}
+	public String getHasDiabetes() {
+		return hasDiabetes;
+	}
+	public void setHasDiabetes(String hasDiabetes) {
+		this.hasDiabetes = hasDiabetes;
+	}
+	public String getHasAlzheimers() {
+		return hasAlzheimers;
+	}
+	public void setHasAlzheimers(String hasAlzheimers) {
+		this.hasAlzheimers = hasAlzheimers;
+	}
+}
